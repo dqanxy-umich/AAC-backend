1e125ae2ea5acc9ff3463c47a44d3af21fae9560
jcgamboa <jcgamboa@umich.edu>
2024-04-13 10:50:10 -0400

    Edited install script

--- bin/install.sh
+++ bin/install.sh
@@ -6,8 +6,10 @@
 set -Eeuo pipefail
 set +x
 
-python3 -m venv env
-source env/bin/activate
+
+# if you want to use a virtual env
+# python3 -m venv env
+# source env/bin/activate
 
 pip install flask
 
