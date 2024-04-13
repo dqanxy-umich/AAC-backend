3c5a5c2652bf7624e4e5d7b8d7ef36a843a4013e
sushi-pita-123 <sushritarakshit@gmail.com>
2024-04-13 10:30:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import google.generativeai as genai
 
 app = Flask(__name__)
 
@@ -10,7 +11,17 @@ def test():
 def page_not_found(error):
     return '404 Not Found', 404
 
+#make token request to Gemini and return next copilot
+@app.route('/copilot')
+def copilot(user_input, input_path):
+  model = genai.GenerativeModel('models/gemini-1.5-pro-latest')
+  new_up = genai.upload_file(path=input_path)
+  prompt_final = f"Finish the following after '...': {user_input}..."
+  response_final = model.generate_content([new_up, prompt_final])
+  result_string = (response_final.text).replace(f'...{user_input}', "")
+  result_string = (response_final.text).replace(f'... {user_input}', "")
+  return result_string
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-    
